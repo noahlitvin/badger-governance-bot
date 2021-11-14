a986f157b505aba1ee5a4cf64b9751aaa6129b29
Noah Litvin <noah.litvin@gmail.com>
2021-11-14 12:42:20 -0500

    ready

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 require('dotenv').config()
-
+const { ethers } = require("ethers")
+const provider = ethers.getDefaultProvider("homestead", { infura: process.env.INFURA_KEY });
+const GovernanceTimelockAddress = '0x21CF9b77F88Adf8F8C98d7E33Fe601DC57bC0893';
+const GovernanceTimelockAbi = require('./GovernanceTimelock.json');
+const GovernanceTimelockContract = new ethers.Contract(GovernanceTimelockAddress, GovernanceTimelockAbi, provider);
 const Discord = require('discord.js')
 const client = new Discord.Client({ intents: [] });
 client.login(process.env.BOT_TOKEN)
 
-const { ethers, utils } = require("ethers")
-const provider = ethers.getDefaultProvider();
-const GovernanceTimelockAddress = '0x21CF9b77F88Adf8F8C98d7E33Fe601DC57bC0893';
-const GovernanceTimelockAbi = require('./GovernanceTimelock.json');
-const GovernanceTimelockContract = new ethers.Contract(GovernanceTimelockAddress, GovernanceTimelockAbi, provider);
-
+// Listen for new events
 GovernanceTimelockContract.on("*", function (eventObject) {
     sendMessage(eventObject);
 });
 
+// Allow test event triggering
 const express = require('express')
 const app = express()
 const port = 8080
@@ -28,6 +28,7 @@ app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}`)
 })
 
+// Send message to Discord
 function sendMessage(eventObject) {
     if (!['QueueTransaction', 'ExecuteTransaction', 'CancelTransaction'].includes(eventObject.event)) {
         return
@@ -50,9 +51,11 @@ function sendMessage(eventObject) {
     const parameters = signature.substring(signature.indexOf('(') + 1, signature.lastIndexOf(')'));
     const functionName = signature.split('(')[0];
     const parameterTypes = parameters.split(',');
-    const decodedParameters = ethers.utils.defaultAbiCoder.decode(parameterTypes, eventObject.data);
+    const decodedParameters = ethers.utils.defaultAbiCoder.decode(parameterTypes, eventObject.args.data);
 
     const message = `Transaction ${eventDescription}: ${functionName}${"(" + decodedParameters.join(", ") + ")"}`
     console.log(message)
-    client.channels.cache.get(process.env.DISCORD_CHANNEL_ID).send(message);
+    client.channels.fetch(process.env.DISCORD_CHANNEL_ID).then(channel =>
+        channel.send(message)
+    )
 }
